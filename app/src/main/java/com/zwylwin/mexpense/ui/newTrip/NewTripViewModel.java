30297463d05dccb668aff0fa55b031a669bf612b
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2022-11-20 15:40:15 +0630

    feat[viewmodel]: Add NewTripViewModel

--- app/src/main/java/com/zwylwin/mexpense/ui/newTrip/NewTripViewModel.java
+++ app/src/main/java/com/zwylwin/mexpense/ui/newTrip/NewTripViewModel.java
@@ -1,7 +1,28 @@
 package com.zwylwin.mexpense.ui.newTrip;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.time.LocalDate;
+
 public class NewTripViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final MutableLiveData<String> tripDestination = new MutableLiveData<String>();
+    private final MutableLiveData<String> tripName = new MutableLiveData<String>();
+    private final MutableLiveData<LocalDate> startDate = new MutableLiveData<>();
+
+    public MutableLiveData<String> getTripDestination() { return tripDestination; }
+
+    public void setTripDestination(String destination) { tripDestination.setValue(destination);}
+
+    public MutableLiveData<String> getTripName() { return tripName; }
+
+    public void setTripName(String name) { tripName.setValue(name); }
+
+    public MutableLiveData<LocalDate> getStartDate() {
+        return startDate;
+    }
+
+    public void setStartLocalDate(LocalDate date) {
+        startDate.setValue(date);
+    }
 }
