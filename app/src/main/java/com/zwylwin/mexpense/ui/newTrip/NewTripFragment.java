2ce70dac985f586c9363bb21c7ff14572f99eca5
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2022-11-20 15:38:56 +0630

    feat[fragment]: Set up life cycle observers and view model

--- app/src/main/java/com/zwylwin/mexpense/ui/newTrip/NewTripFragment.java
+++ app/src/main/java/com/zwylwin/mexpense/ui/newTrip/NewTripFragment.java
@@ -7,34 +7,65 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.text.Editable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-import com.google.android.material.textfield.TextInputLayout;
 import com.zwylwin.mexpense.databinding.FragmentNewTripBinding;
+import com.zwylwin.mexpense.ui.datepicker.DatePickerFragment;
 
 import java.util.Objects;
 
 public class NewTripFragment extends Fragment {
     private FragmentNewTripBinding binding;
-    private EditText newTripDate;
     private EditText newTripDestination;
     private EditText newTripName;
+    private EditText newTripStartDate;
+    private NewTripViewModel newTripViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        newTripViewModel = new ViewModelProvider(requireActivity()).get(NewTripViewModel.class);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NewTripViewModel newTripViewModel =
-                new ViewModelProvider(this).get(NewTripViewModel.class);
         binding = FragmentNewTripBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        newTripName = binding.newTripName.getEditText();
+        newTripDestination = Objects.requireNonNull(binding.newTripDestination.getEditText());
+        newTripName = Objects.requireNonNull(binding.newTripName.getEditText());
+        newTripStartDate = Objects.requireNonNull(binding.newTripStartDate.getEditText());
 
-        return root;
+        newTripDestination.setOnFocusChangeListener((view, hasFocus) -> {
+            newTripViewModel.setTripDestination(newTripDestination.getText().toString());
+        });
+
+        newTripName.setOnFocusChangeListener((view, hasFocus) -> {
+            newTripViewModel.setTripName(newTripName.getText().toString());
+        });
+
+        newTripStartDate.setOnClickListener(view -> {
+            DatePickerFragment datePickerFragment = new DatePickerFragment();
+            datePickerFragment.show(requireActivity().getSupportFragmentManager(), "startDatePicker");
+        });
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        newTripViewModel.getStartDate().observe(getViewLifecycleOwner(), date -> {
+            newTripStartDate.setText(date.toString());
+        });
+        newTripViewModel.getTripName().observe(getViewLifecycleOwner(), name -> {
+            newTripName.setText(name.toString());
+        });
+        newTripViewModel.getTripDestination().observe(getViewLifecycleOwner(), destination -> {
+            newTripDestination.setText(destination.toString());
+        });
     }
 
     @Override
